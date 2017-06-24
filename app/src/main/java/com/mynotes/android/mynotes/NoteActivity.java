2b2313dd9bfef220c8db015e160731b0d0dc87d8
Guillermo Ortiz Vitali <ortiz.vitali.guillermo@gmail.com>
2017-06-24 19:55:34 -0300

    add menu in noteActivity

--- app/src/main/java/com/mynotes/android/mynotes/NoteActivity.java
+++ app/src/main/java/com/mynotes/android/mynotes/NoteActivity.java
@@ -9,8 +9,11 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mynotes.android.mynotes.data.NotesContract;
 import com.mynotes.android.mynotes.data.NotesDbHelper;
@@ -106,7 +109,6 @@ public class NoteActivity extends AppCompatActivity {
 
 
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingSaveActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -160,6 +162,27 @@ public class NoteActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id=item.getItemId();
+        if (id==R.id.action_attach){
+
+            Toast.makeText(this, "Attaching img comming soon!", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     void handleSendText(Intent intent) {
